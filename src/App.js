7da43c56fbe82058a2e6c035d1d5951508c8df04
Vincent Portier <vinz.portier@gmail.com>
2020-08-13 18:24:45 +1000

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { useState } from "react";
+import { useEffect } from "react";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-function App() {
+library.add(fab);
+
+const App = () => {
+  const [quotes, setQuotes] = useState([]);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [text, setText] = useState("");
+  const [author, setAuthor] = useState("");
+
+  let randomQuote = Math.floor(Math.random() * 1642);
+
+  useEffect(() => {
+    fetch("https://type.fit/api/quotes")
+      .then((res) => res.json())
+      .then((json) => {
+        setQuotes(json);
+        setIsLoaded(true);
+      });
+
+    if (isLoaded === true) {
+      setText(quotes[randomQuote].text);
+      setAuthor(quotes[randomQuote].author);
+    }
+  }, [isLoaded]);
+
+  const getNewQuote = () => {
+    setText(quotes[randomQuote].text);
+    setAuthor(quotes[randomQuote].author);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="app__quote" id="quote-box">
+        <h3 className="quote__text" id="text">
+          {`"${text}"`}
+          <span className="quote__author" id="author">
+            {author !== null ? ` ${author}` : ""}
+          </span>
+        </h3>
+
+        <div className="quote__controls">
+          <button className="btn" id="new-quote" onClick={getNewQuote}>
+            New quote
+          </button>
+          <button className="btn">
+            <a
+              id="tweet-quote"
+              href={`https://twitter.com/intent/tweet?text=${text} ${author}`}
+              target="popup"
+            >
+              <FontAwesomeIcon
+                icon={["fab", "twitter"]}
+                size="lg"
+                style={{ marginRight: 5 }}
+              ></FontAwesomeIcon>
+              {`Tweet `}
+            </a>
+          </button>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
